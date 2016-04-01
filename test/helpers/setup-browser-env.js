72b0e468a3724b515b58a951cf80663ff82f1098
George Crabtree <supercrabtree@googlemail.com>
2016-04-01 17:03:27 +0200

    Add html and document / window globals

---
+++ test/helpers/setup-browser-env.js
@@ -0,0 +1,37 @@
+var jsdom = require('jsdom').jsdom;
+
+// create a small html tree to test against
+
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Hi Mum!</title>
+</head>
+<body>
+
+  <h1>Hi Mum!</h1>
+
+  <section>
+
+    <h2>Countries</h2>
+
+    <div class="countries">
+      <ul>
+        <li>Germany</li>
+        <li>Belize</li>
+        <li>France</li>
+        <li>Australia</li>
+        <li>India</li>
+      </ul>
+    </div>
+  </section>
+
+</body>
+</html>`
+
+// create the document and expose it globally for the tests
+global.document = jsdom(html);
+global.window = document.defaultView;
+
