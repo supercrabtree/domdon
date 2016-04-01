cea923b2a371a9e3aa97855e3e4fd9b04a4c5af8
George Crabtree <supercrabtree@googlemail.com>
2016-04-01 17:07:08 +0200

    Add basic selector tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 import test from 'ava';
 import DOM from '../index.js';
 
+test("DOM('h1') returns the h1 element", t => {
+  t.same(window.HTMLHeadingElement, DOM('h1').constructor);
+});
 
-test(t => {
-  t.true(true);
+test("DOM('li') returns a Array", t => {
+  t.same(Array, DOM('li').constructor);
 });
