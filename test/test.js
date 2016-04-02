d046d85575d81d31674638eef2f8974502df54b7
George Crabtree <supercrabtree@googlemail.com>
2016-04-03 00:16:44 +0200

    Add whitespace test

--- test/test.js
+++ test/test.js
@@ -7,6 +7,12 @@ test("DOM('') returns null", t => {
   t.same(null, DOM(''));
 });
 
+test("DOM() call with just whitespace returns null", t => {
+  t.same(null, DOM(' '));
+  t.same(null, DOM('   '));
+  t.same(null, DOM('  \t  \n  '));
+});
+
 test("DOM('h1') returns the h1 element", t => {
   t.same(window.HTMLHeadingElement, DOM('h1').constructor);
 });
