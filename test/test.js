08d4c4af0ea235b431412bd4f63b948a5606415e
George Crabtree <supercrabtree@googlemail.com>
2016-04-04 13:21:27 +0200

    Fix typo

--- test/test.js
+++ test/test.js
@@ -61,7 +61,7 @@ test("DOM('h1') returns the h1 element", t => {
   t.same(window.HTMLHeadingElement, DOM('h1').constructor);
 });
 
-test("DOM('li') returns a Array", t => {
+test("DOM('li') returns an Array", t => {
   t.same(Array, DOM('li').constructor);
 });
 
